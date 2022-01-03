0b9ac9142b3aa61d4c39e31dc705f12caafb6fcf
CUI <garycui222@gmail.com>
2022-01-03 00:01:47 -0500

    fix container margin position

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,9 +1,5 @@
 /* eslint-disable no-undef */
 $(document).ready(function() {
-  
-  // $("#btn").on('click', function() {
-  //   console.log(this); //The this keyword is a reference to the button
-  // });
 
   let maxLength = 140;
   $('#tweet-text').keyup(function() {
@@ -12,8 +8,14 @@ $(document).ready(function() {
     let newLength = maxLength - length;
     if (newLength < 0) {
       $('output').addClass("overInput");
+    } else {
+      $('output').removeClass("overInput");
     }
     $('output').text(newLength);
     
+  });
+
+  $('#resetCount').click(function() {
+    $('output').text(maxLength);
   });
 });
